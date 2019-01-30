ffd13f3b22a425eb5476be6152cc476998d26197
Raul Gonzalez <raulg42@gmail.com>
2019-01-30 13:00:51 +0100

    Demo repo
    Setting up a demo repository

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -1,6 +1,6 @@
 export default {
 
-  apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
+  apiEndpoint: 'https://bambi-blog-php.prismic.io/api/v2',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
