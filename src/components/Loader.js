99a0bce8b8751a859f6b1a0e3b402758752fab00
Levi Gable <levimykel@gmail.com>
2019-11-13 16:15:27 +0100

    Add Loader to the Preview component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div id="loader">
+      <div className="lds-ripple">
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
