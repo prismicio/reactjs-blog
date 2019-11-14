7d7e528371b1415f1646b33e1a5a91e63ac039d4
Levi Gable <levimykel@gmail.com>
2019-11-14 17:16:40 +0100

    Remove meta title from 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,18 +4,14 @@ import { Link } from 'react-router-dom';
 /**
  * Not found (404) component
  */
-const NotFound = () => {
-  document.title = '404 Error';
-
-  return (
-    <div className="not-found">
-      <h1>404</h1>
-      <h2>Document not found</h2>
-      <p>
-        <Link to="/">Return to homepage</Link>
-      </p>
-    </div>
-  );
-}
+const NotFound = () => (
+  <div className="not-found">
+    <h1>404</h1>
+    <h2>Document not found</h2>
+    <p>
+      <Link to="/">Return to homepage</Link>
+    </p>
+  </div>
+);
 
 export default NotFound;
