f83e3478a9c240043a2b00af6ecae1043358f4c2
Levi Gable <levimykel@gmail.com>
2019-11-14 15:38:39 +0100

    Add serializeHyperlink to text slice component

--- src/components/slices/Text.js
+++ src/components/slices/Text.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { shape, array } from 'prop-types'
 import { RichText } from 'prismic-reactjs'
+import { customLink } from '../../utils/prismicHelpers'
 
 const Text = ({ slice, prismicCtx }) => {
   return (
     <div className='post-part single container'>
       <div>
-        <RichText render={slice.primary.text} linkResolver={prismicCtx.linkResolver} />
+        <RichText
+          render={slice.primary.text}
+          linkResolver={prismicCtx.linkResolver}
+          serializeHyperlink={customLink}
+        />
       </div>
     </div>
   )
