da4c645b2d812a92656c947296dbfa85b7406735
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-12-02 13:10:44 -1000

    feat: update failed preview resolver message

--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -9,12 +9,13 @@ import { Loader } from "../components/Loader";
  */
 export const Preview = () => {
   const navigate = useNavigate();
-  const previewURL = usePrismicPreviewResolver({ navigate });
+  const [previewURL, previewState] = usePrismicPreviewResolver({ navigate });
 
   useEffect(() => {
-    if (!previewURL) {
-      return console.warn(`Unable to retrieve the preview token from provided url. \n
-      Check https://prismic.io/docs/reactjs/beyond-the-api/in-website-preview for more info`);
+    if (previewState.state === "failed") {
+      return console.warn(
+        `Unable to resolve a preview from the current URL.\nCheck https://prismic.io/docs/reactjs/beyond-the-api/in-website-preview for more info`
+      );
     }
   }, [previewURL]);
 
